9356357abc95acf2c01bf9658ad31d5ca31e0ac0
unknown <korominsta98@gmail.com>
2024-07-24 13:15:45 +0800

    please work

--- components/QuoteCard.jsx
+++ components/QuoteCard.jsx
@@ -71,7 +71,7 @@ const QuoteCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
             />
             <div className="flex flex-col">
               <h3 className="ml-2 font-satoshi text-base font-semibold text-gray-900">{post.creator.username}</h3>
-              
+              {/* <p className="font-inter text-sm text-gray-500">{post.creator.email}</p> */}
             </div>
           </div>
           <div className="copy-btn hover:cursor-pointer" onClick={handleCopy}>
